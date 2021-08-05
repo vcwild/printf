7dc3ce30d4f9e881590d23202c8011adf9b096e5
Victor <vcwildm@gmail.com>
2021-08-05 06:36:18 -0300

    update argument format handler

--- handlers/handle_argument_format.c
+++ handlers/handle_argument_format.c
@@ -2,5 +2,22 @@
 
 int handle_argument_format(int c, va_list args)
 {
-	// TODO: Implement this, see the 42 documentation for details.
+	int counter;
+
+	counter = 0;
+	if (c == 'c')
+		counter = handle_char(va_arg(args, int));
+	if (c == 's')
+		counter = handle_string(va_arg(args, char *));
+	if (c == 'p')
+		counter = handle_pointer(va_arg(args, long unsigned));
+	if (c == 'i' || c == 'd')
+		counter = handle_integer(va_arg(args, signed int));
+	if (c == 'u')
+		counter = handle_unsigned_decimal(va_arg(args, unsigned int));
+	if (c == 'x')
+		counter = handle_hexadecimal(va_arg(args, unsigned int));
+	if (c == '%')
+		counter = handle_percent(c);
+	return (counter);
 }
