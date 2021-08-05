0a4c034155924f8e32817163ecd1e6fcb30ec932
Victor <vcwildm@gmail.com>
2021-08-05 06:37:36 -0300

    update helpers header

--- handlers/helpers/helpers.h
+++ handlers/helpers/helpers.h
@@ -1,10 +1,14 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-#include <unistd.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-int	ft_putchar(char c);
+int		ft_putchar(char c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 void	ft_putnbr_base(int nbr, char *base);
 void	ft_putnbr(int nbr);
+char	*itoa_base(unsigned long long n, char *base);
 
 #endif
