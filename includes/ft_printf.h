aaa6eba5b3764249b52ed1849b928be19b9dc191
Victor <vcwildm@gmail.com>
2021-08-12 10:04:02 -0300

    chore: move header dependencies to `ft_printf.h`

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,25 +1,26 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *fmt, ...);
-
-#define	HEX_BASE		"0123456789abcdef"
-#define	DECIMAL_BASE	"0123456789"
-#define	PRECEDING_STR	"0x"
-
-#include <stdarg.h>
-
-int	handle_argument_format(int c, va_list args);
-int	handle_char(char c);
-int	handle_string(char *str);
-int	handle_pointer(long unsigned ptr_addr);
-int	handle_integer(signed int value);
-int	handle_unsigned_decimal(unsigned int value);
-int	handle_hexadecimal(unsigned int value);
-int	handle_percent(char c);
-
+# include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+
+# define	HEX_BASE			"0123456789abcdef"
+# define	HEX_BASE_UPPER		"0123456789ABCDEF"
+# define	DECIMAL_BASE		"0123456789"
+# define	PRECEDING_STR		"0x"
+
+int		ft_printf(const char *fmt, ...);
+
+int		handle_argument_format(int c, va_list args);
+int		handle_char(char c);
+int		handle_string(char *str);
+int		handle_pointer(long unsigned ptr_addr);
+int		handle_decimal(signed long value);
+int		handle_unsigned_decimal(unsigned int value);
+int		handle_hexadecimal(unsigned int value);
+int		handle_hexadecimal_upper(unsigned int value);
+int		handle_percent(char c);
 
 int		ft_putchar(char c);
 size_t	ft_strlen(const char *s);
@@ -28,8 +29,7 @@ void	ft_putnbr_base(int nbr, char *base);
 void	ft_putnbr(int nbr);
 char	*itoa_base(unsigned long long n, char *base);
 
-int	is_argument(int c);
-int is_flag(int c);
-
+int		is_argument(int c);
+int		is_flag(int c);
 
 #endif
